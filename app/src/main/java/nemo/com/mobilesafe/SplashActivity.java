e1d2d79ed794bd7dc621811a135b43fb19045004
nemo <ykyu112358@126.com>
2016-06-04 00:26:32 +0800

    add the update process of app

--- app/src/main/java/nemo/com/mobilesafe/SplashActivity.java
+++ app/src/main/java/nemo/com/mobilesafe/SplashActivity.java
@@ -3,14 +3,28 @@ package nemo.com.mobilesafe;
 import android.app.Activity;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import nemo.com.mobilesafe.nemo.com.mobilesafe.utils.StreamTool;
 
 /**
  * Created by nemo on 16-6-2.
  */
 public class SplashActivity extends Activity {
+    private static final String TAG = "SplashActivity";
     private TextView tvSplashVersion = null;
+    private Button btnCheckUpdate = null;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +32,48 @@ public class SplashActivity extends Activity {
 
         tvSplashVersion = (TextView) findViewById(R.id.tv_splash_version);
         tvSplashVersion.setText("版本号：" + getVersionName());
+        btnCheckUpdate = (Button) findViewById(R.id.btn_checkupdate);
+        btnCheckUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestUpate();
+            }
+        });
     }
 
-    public String getVersionName() {
+    private void requestUpate() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                URL url = null;
+                HttpURLConnection conn = null;
+
+                try {
+                    url = new URL(getString(R.string.updateurl));
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setConnectTimeout(4000);
+                    int returnCode = conn.getResponseCode();
+
+                    if(200 == returnCode) {
+                        Log.i(TAG, "connect to web successful!");
+                        InputStream is = conn.getInputStream();
+                        String result = StreamTool.getStringFormStream(is);
+
+                        Log.i(TAG, "The result is: " + result);
+                    }
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+    }
+
+    private String getVersionName() {
         PackageManager pm = getPackageManager();
 
         try {
---
+++ app/src/main/java/nemo/com/mobilesafe/nemo/com/mobilesafe/utils/StreamTool.java
@@ -0,0 +1,37 @@
+package nemo.com.mobilesafe.nemo.com.mobilesafe.utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by nemo on 16-6-3.
+ */
+public class StreamTool {
+    public static String getStringFormStream(InputStream is) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+
+        int len = -1;
+
+        try {
+            while((len = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+
+            return baos.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return "";
+    }
+}
