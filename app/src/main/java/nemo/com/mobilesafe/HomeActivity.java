b15669f66170812fb6a09886dcb29aea8db9f2ba
nemo <ykyu112358@126.com>
2016-06-07 00:38:32 +0800

    add the progress of download the update app

---
+++ app/src/main/java/nemo/com/mobilesafe/HomeActivity.java
@@ -0,0 +1,15 @@
+package nemo.com.mobilesafe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nemo on 16-6-5.
+ */
+public class HomeActivity extends Activity {
+    public void onCreate(Bundle savedInstatnceSate) {
+        super.onCreate(savedInstatnceSate);
+        setContentView(R.layout.home_activity);
+
+    }
+}
--- app/src/main/java/nemo/com/mobilesafe/SplashActivity.java
+++ app/src/main/java/nemo/com/mobilesafe/SplashActivity.java
@@ -1,15 +1,34 @@
 package nemo.com.mobilesafe;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import net.tsz.afinal.FinalHttp;
+import net.tsz.afinal.http.AjaxCallBack;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -23,8 +42,52 @@ import nemo.com.mobilesafe.nemo.com.mobilesafe.utils.StreamTool;
  */
 public class SplashActivity extends Activity {
     private static final String TAG = "SplashActivity";
+    private static final int SHOWUPDATEDIALOG = 0;
+    private static final int ENTERHOME = 1;
+    private static final int URL_ERROR = 2;
+    private static final int NETWORK_ERROR = 3;
+    private static final int JSON_ERROR = 4;
+
     private TextView tvSplashVersion = null;
-    private Button btnCheckUpdate = null;
+    private ProgressBar pbUpdate = null;
+
+    private String description = null;
+    private String apkUrl = null;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what) {
+                case SHOWUPDATEDIALOG: {
+                    showUpdateDialog();
+                    break;
+                }
+                case ENTERHOME: {
+                    enterHome();
+                    finish();
+                    break;
+                }
+                case URL_ERROR: {
+                    Toast.makeText(SplashActivity.this, "URL ERROR!", Toast.LENGTH_SHORT).show();
+                    enterHome();
+                    finish();
+                    break;
+                }
+                case NETWORK_ERROR: {
+                    Toast.makeText(SplashActivity.this, "NETWORK ERROR!", Toast.LENGTH_SHORT).show();
+                    enterHome();
+                    finish();
+                    break;
+                }
+                case JSON_ERROR: {
+                    Toast.makeText(SplashActivity.this, "JSON ERROR!", Toast.LENGTH_SHORT).show();
+                    enterHome();
+                    finish();
+                    break;
+                }
+            }
+        }
+    };
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,13 +95,85 @@ public class SplashActivity extends Activity {
 
         tvSplashVersion = (TextView) findViewById(R.id.tv_splash_version);
         tvSplashVersion.setText("版本号：" + getVersionName());
-        btnCheckUpdate = (Button) findViewById(R.id.btn_checkupdate);
-        btnCheckUpdate.setOnClickListener(new View.OnClickListener() {
+        requestUpate();
+
+        pbUpdate = (ProgressBar) findViewById(R.id.pb_updateprogress);
+
+//        AlphaAnimation aa = new AlphaAnimation(0.2f, 0.1f);
+//        aa.setDuration(1000);
+//        findViewById(R.id.rl_splash).startAnimation(aa);
+    }
+
+    public void showUpdateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+        builder.setTitle(R.string.updatenotice);
+        builder.setMessage(description);
+        builder.setCancelable(false);
+
+        builder.setPositiveButton(R.string.updatepositive, new DialogInterface.OnClickListener() {
+
+
             @Override
-            public void onClick(View v) {
-                requestUpate();
+            public void onClick(DialogInterface dialog, int which) {
+
+                Log.i(TAG, "OnClick Positive Button");
+                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                    Log.i(TAG, "Begin download apk and update the app!");
+
+                    pbUpdate.setVisibility(View.VISIBLE);
+
+                    FinalHttp finalHttp = new FinalHttp();
+                    finalHttp.download(apkUrl, Environment.getExternalStorageDirectory().getAbsolutePath() +
+                            "/mobileSafe2.0.apk", new AjaxCallBack<File>() {
+                        @Override
+                        public void onLoading(long count, long current) {
+                            super.onLoading(count, current);
+                            pbUpdate.setMax((int) count);
+                            pbUpdate.setProgress((int) current);
+                        }
+
+                        @Override
+                        public void onSuccess(File file) {
+                            Toast.makeText(SplashActivity.this, "download successful!", Toast.LENGTH_SHORT).show();
+                            installApk(file);
+                            super.onSuccess(file);
+                        }
+
+                        @Override
+                        public void onFailure(Throwable t, int errorNo, String strMsg) {
+                            t.printStackTrace();
+                            Toast.makeText(SplashActivity.this, "download fail!", Toast.LENGTH_SHORT).show();
+                            super.onFailure(t, errorNo, strMsg);
+
+                        }
+
+                        private void installApk(File file) {
+                            Intent intent = new Intent();
+                            intent.setAction("android.intent.action.VIEW");
+                            intent.addCategory("android.intent.category.DEFAULT");
+                            intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+
+                            startActivity(intent);
+
+                        }
+                    });
+                }
+                dialog.dismiss();
+                dialog = null;
+                finish();
             }
         });
+        builder.setNegativeButton(R.string.updatenegative, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                dialog = null;
+                enterHome();
+                finish();
+            }
+        });
+
+        builder.show();
     }
 
     private void requestUpate() {
@@ -47,6 +182,8 @@ public class SplashActivity extends Activity {
             protected Void doInBackground(Void... params) {
                 URL url = null;
                 HttpURLConnection conn = null;
+                Message mes = handler.obtainMessage();
+                long startTime = System.currentTimeMillis();
 
                 try {
                     url = new URL(getString(R.string.updateurl));
@@ -60,13 +197,40 @@ public class SplashActivity extends Activity {
                         InputStream is = conn.getInputStream();
                         String result = StreamTool.getStringFormStream(is);
 
-                        Log.i(TAG, "The result is: " + result);
+                        JSONObject jsonObject = new JSONObject(result);
+                        String version = jsonObject.getString("version");
+                        description = jsonObject.getString("description");
+                        apkUrl = jsonObject.getString("apkurl");
+
+                        Log.i(TAG, "version: " + version +
+                                ", description: " + description +
+                                ", apkurl: " + apkUrl);
+
+                        if(getVersionName().equals(version)) {
+                            mes.what = ENTERHOME;
+                        } else {
+                            mes.what = SHOWUPDATEDIALOG;
+                        }
                     }
 
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
+                    mes.what = URL_ERROR;
                 } catch (IOException e) {
                     e.printStackTrace();
+                    mes.what = NETWORK_ERROR;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    mes.what = JSON_ERROR;
+                } finally {
+                    long endTime = System.currentTimeMillis();
+                    long durtion = endTime - startTime;
+
+//                    if(durtion < 2000) {
+//                        SystemClock.sleep(2000-durtion);
+//                    }
+
+                    mes.sendToTarget();
                 }
                 return null;
             }
@@ -84,5 +248,10 @@ public class SplashActivity extends Activity {
         }
 
         return "";
+    }
+
+    private void enterHome() {
+        Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
