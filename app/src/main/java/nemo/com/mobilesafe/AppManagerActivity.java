403f5695954f7ff17f92dad0b0529f3651a959cb
nemo <ykyu112358@126.com>
2016-07-26 23:52:25 +0800

    change some things

--- app/src/main/java/nemo/com/mobilesafe/AppManagerActivity.java
+++ app/src/main/java/nemo/com/mobilesafe/AppManagerActivity.java
@@ -1,6 +1,7 @@
 package nemo.com.mobilesafe;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StatFs;
@@ -11,8 +12,10 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import nemo.com.mobilesafe.domain.AppInfo;
@@ -29,6 +32,8 @@ public class AppManagerActivity extends Activity {
     private ListView lvAppInfos;
 
     private List<AppInfo> appInfoList;
+    private List<AppInfo> userAppInfoList;
+    private List<AppInfo> systemAppInfoList;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +63,17 @@ public class AppManagerActivity extends Activity {
             public void run() {
                 llLoading.setVisibility(View.VISIBLE);
                 appInfoList = AppInfoProvider.getAppInfos(AppManagerActivity.this);
+                userAppInfoList = new ArrayList<AppInfo>();
+                systemAppInfoList = new ArrayList<AppInfo>();
+
+                for(AppInfo appInfo : appInfoList) {
+                    if(appInfo.isUserApp()) {
+                        userAppInfoList.add(appInfo);
+                    } else {
+                        systemAppInfoList.add(appInfo);
+                    }
+                }
+
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -89,12 +105,35 @@ public class AppManagerActivity extends Activity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            AppInfo appInfo = appInfoList.get(position);
+            AppInfo appInfo = null;
 
             View view = null;
             ViewHolder viewHolder = null;
 
-            if(convertView == null) {
+            if(position == 0) {
+                TextView tv = new TextView(getApplicationContext());
+                tv.setTextColor(Color.WHITE);
+                tv.setBackgroundColor(Color.GRAY);
+                tv.setText("用户软件：" + userAppInfoList.size() + "个");
+                return tv;
+            } else if(position == (userAppInfoList.size() + 1)) {
+                TextView tv = new TextView(getApplicationContext());
+                tv.setTextColor(Color.WHITE);
+                tv.setBackgroundColor(Color.GRAY);
+                tv.setText("系统软件：" + systemAppInfoList.size() + "个");
+                return tv;
+            } else if(position <= userAppInfoList.size()) {
+                int newPosition = position -1;
+                appInfo = userAppInfoList.get(newPosition);
+            } else {
+                int newPosition = position - 1 -userAppInfoList.size() -1;
+                appInfo = systemAppInfoList.get(newPosition);
+            }
+
+            if(convertView != null && convertView instanceof RelativeLayout) {
+                view = convertView;
+                viewHolder = (ViewHolder) view.getTag();
+            } else {
                 view = View.inflate(getApplicationContext(), R.layout.list_item_appinfo, null);
                 viewHolder = new ViewHolder();
                 viewHolder.tvName = (TextView) view.findViewById(R.id.tv_app_name);
@@ -102,9 +141,6 @@ public class AppManagerActivity extends Activity {
                 viewHolder.ivIcon = (ImageView) view.findViewById(R.id.iv_app_icon);
                 viewHolder.ivStatus = (ImageView) view.findViewById(R.id.iv_status);
                 view.setTag(viewHolder);
-            } else {
-                view = convertView;
-                viewHolder = (ViewHolder) view.getTag();
             }
 
             viewHolder.ivIcon.setImageDrawable(appInfo.getIcon());
